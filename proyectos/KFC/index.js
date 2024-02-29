e3cde977ec4ffe8723d31d4b68cc156257a12e77
Mateo Velez <mateo.velez.censa@gmail.com>
2024-02-28 19:06:25 -0500

    Se arregla la dirección de las hamburguesas

--- proyectos/KFC/index.js
+++ proyectos/KFC/index.js
@@ -24,7 +24,7 @@ function cambiarTemas(){
 Volver.addEventListener("click",()=>{
     let confirmar = confirm("Quieres regresar al menu?");
     if(confirmar === true){
-        window.location.href = "../index.html";
+        window.location.href ="../../index.html";
     }
 })
 
